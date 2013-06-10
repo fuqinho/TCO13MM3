763703fc929b5264ae0ad9e00a76fac1647cb71a
fuqinho <kitakita7@gmail.com>
2013-06-10 16:26:38 +0900

    try 6

--- xcode/CirclesSeparation.h
+++ xcode/CirclesSeparation.h
@@ -38,11 +38,12 @@ const double INF = 1e100;
 const double G = 9.8;
 const double E = 0.0;
 const double TIME_PER_FRAME = 0.001;
-const int RESTART_FRAME = 10000;
+const int RESTART_FRAME = 12000;
 
 #define MAX_CIRCLE_NUM 512
 pair<double, pair<int, bool> > xs[MAX_CIRCLE_NUM * 2];
-pair<int, int> pairs[MAX_CIRCLE_NUM * 100];
+pair<int, int> pairs[MAX_CIRCLE_NUM * 8];
+double m_mul_div_sum[MAX_CIRCLE_NUM][MAX_CIRCLE_NUM];
 
 #define PROFILE 0
 class Profiler {
@@ -50,33 +51,25 @@ public:
     int last;
     long long acc[100];
     Profiler() {
-#if PROFILE
         memset(acc, 0, sizeof(acc));
-#endif
     }
     void start() {
-#if PROFILE
         struct timeval  tv;
         gettimeofday(&tv, NULL);
         last = tv.tv_usec;
-#endif
     }
     void end(int id) {
-#if PROFILE
         struct timeval  tv;
         gettimeofday(&tv, NULL);
         int elapsed = (tv.tv_usec - last + 1000000) % 1000000;
         acc[id] += elapsed;
-#endif
     }
     void report() {
-#if PROFILE
         for (int i = 0; i < 100; i++) if (acc[i] > 0) {
             cerr << i << ": " << acc[i] / 1000.0 << "(ms), ";
         }
         cerr << endl;
         memset(acc, 0, sizeof(acc));
-#endif
     }
 };
 
@@ -91,8 +84,9 @@ struct Circle {
     double r, m;
     Vec o_pos, pos, v;
     bool is_hover;
-    double cost;
     double inv_r2;
+    double inv_m;
+    double gravity;
 };
 
 bool greaterMass(const Circle& lhs, const Circle& rhs) {
@@ -130,10 +124,18 @@ public:
             c[i].m = m[i];
             c[i].is_hover = false;
             c[i].inv_r2 = 1.0 / r[i] / r[i];
+            c[i].inv_m = 1.0 / m[i];
         }
         
         // determine initial position
         sort(c.begin(), c.end(), greaterMass);
+
+        // precompute
+        for (int i = 0; i < N; i++) for (int j = i; j < N; j++) {
+            m_mul_div_sum[i][j] = m_mul_div_sum[j][i] = c[i].m * c[j].m / (c[i].m + c[j].m);
+            c[i].gravity = G * TIME_PER_FRAME * min(3.0, 1 + 0.01 * c[i].m * c[i].inv_r2);
+        }
+
         restart();
     }
     
@@ -156,17 +158,7 @@ public:
         
         sort(scores.begin(), scores.end());
         
-        /*
-        int ub = N / 5 * totalArea;
-        int MN = (int)(ub * (double)rand() / RAND_MAX);
-        if (frames == 0) MN = N / 10 * totalArea;
-        for (int i = 0; i < min(N, MN); i++) {
-            int index = scores[i].second;
-            Vec d = c[index].pos - Vec(0.5, 0.5);
-            d.normalize();
-            c[index].pos += d * 2;
-        }
-         */
+        // adjust initial filling rate
         double thresh = 0.8 + 0.4 * (double)rand() / RAND_MAX;
         double filledArea = 0.0;
         vector<pair<double, int> > outers;
@@ -174,11 +166,6 @@ public:
             int index = scores[i].second;
             if (filledArea > thresh) {
                 outers.push_back(pair<double, int>(-c[i].m * c[i].inv_r2, index));
-                /*
-                Vec d = c[index].pos - Vec(0.5, 0.5);
-                d.normalize();
-                c[index].pos += d * 5;
-                 */
             } else {
                 filledArea += c[index].r * c[index].r * 3.14;
             }
@@ -196,18 +183,24 @@ public:
         
         ////////////////////////////////
         // apply force
+#if PROFILE
         prof.start();
+#endif
         for (int i = 0; i < N; i++) {
             Vec d = c[i].o_pos - c[i].pos;
             d.normalize();
-            double mul = G * TIME_PER_FRAME * min(3.0, 1 + 0.01 * c[i].m * c[i].inv_r2);
-            c[i].v += d * mul;
+            //double mul = G * TIME_PER_FRAME * min(3.0, 1 + 0.01 * c[i].m * c[i].inv_r2);
+            c[i].v += d * c[i].gravity;
         }
+#if PROFILE
         prof.end(0);
-
+#endif
+        
         ////////////////////////////////
         // broad phase
+#if PRIFLE
         prof.start();
+#endif
         int num_xs = 0;
         for (int i = 0; i < N; i++) {
             xs[num_xs].first = c[i].pos.x - c[i].r - 0.001;
@@ -236,44 +229,53 @@ public:
                 }
             }
         }
+#if PROFILE
         prof.end(1);
+#endif
         
         ////////////////////////////////
         // detect collision and solve constraints
+#if PROFILE
         prof.start();
-        double CD = 0.6 / TIME_PER_FRAME;
+#endif
+        const double CD = 0.6 / TIME_PER_FRAME;
         for (int k = 0; k < num_pairs; k++) {
             int i = pairs[k].first;
             int j = pairs[k].second;
             if (c[i].is_hover || c[j].is_hover) continue;
             Vec d = c[j].pos - c[i].pos;
-            
             if (d.isSmallerThan(c[i].r + c[j].r + 0.001)) {
                 Vec dv = c[j].v - c[i].v;
-                Vec norm = d;
-                norm.normalize();
-                double D = max(0.0, c[i].r + c[j].r  + 0.001 - d.length());
-                double C = c[i].m * c[j].m / (c[i].m + c[j].m) * ((1 + E) * dot(dv, norm) - CD*D);
-                c[i].v += norm * C / (c[i].m);
-                c[j].v -= norm * C / (c[j].m);
+                double len = d.length();
+                Vec norm = d / len;
+                double CDD = CD * (c[i].r + c[j].r  + 0.001 - len);
+                double C = m_mul_div_sum[i][j] * ((1 + E) * dot(dv, norm) - CDD);
+                c[i].v += norm * C * c[i].inv_m;
+                c[j].v -= norm * C * c[j].inv_m;
             }
         }
+#if PROFILE
         prof.end(2);
+#endif
         
         // add friction
         for (int i = 0; i < N; i++) {
             c[i].v *= 0.98;
         }
-    
+
         ////////////////////////////////
         // integrate
+#if PROFILE
         prof.start();
+#endif
         for (int i = 0; i < N; i++) {
             c[i].pos += c[i].v * TIME_PER_FRAME;
         }
+#if PROFILE
         prof.end(4);
+#endif
         
-        
+        // kick circles which have overlap
         if (frames % 300 == 0) {
             for (int i = N-1; i >= 0; i--) {
                 for (int j = 0; j < i; j++) {
@@ -288,17 +290,19 @@ public:
             }
         }
         
-        
         if (frames % 100 == 0)
             updateBest();
+
         
         if (frames % RESTART_FRAME == 0) {
             restart();
         }
         
+#if PROFILE
         if (frames % 10000 == 0) {
             prof.report();
         }
+#endif
     }
     
     double currentCost() {
@@ -344,8 +348,8 @@ public:
     
     vector<double> minimumWork(vector<double> x, vector<double> y, vector<double> r, vector<double> m)
     {
-        //const double MAX_SEC = 9.5;
-        const double MAX_SEC = 1.4;
+        const double MAX_SEC = 9.5;
+        //const double MAX_SEC = 1.4;
         clock_t end__ = clock() + MAX_SEC * CLOCKS_PER_SEC;
         
         setup(x, y, r, m);
@@ -367,6 +371,7 @@ private:
         if (i == j) return false;
         return (c[i].pos - c[j].pos).isSmallerThan(c[i].r + c[j].r);
     }
+    
     
     bool hasOverlap() {
         for (int i = 0; i < N; i++)
